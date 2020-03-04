059e297c19941f56fdd5f697926d09e6d84a7313
Nolan Kovacik <noltron000@gmail.com>
2020-03-04 12:04:31 -0800

    [upd] improved helper functions

--- script.js
+++ script.js
@@ -15,10 +15,6 @@ const loadJSON = (path, callback) => {
 	xobj.send(null)
 }
 
-const addTextToEl = (el, text) => {
-	el.textContent = text
-}
-
 // Learned to apply optional parameters from these sources:
 // https://simonsmith.io/destructuring-objects-as-function-parameters-in-es6
 // https://stackoverflow.com/questions/11796093/is-there-a-way-to-provide-named-parameters-in-a-function-call-in-javascript
@@ -27,30 +23,38 @@ const populateEls = ({
 	numCountriesText = '',
 } = {}) => {
 	const elYear = document.getElementById('data-year')
-	addTextToEl(elYear, yearText)
 	const elNumCountries = document.getElementById('data-num-countries')
-	addTextToEl(elNumCountries, numCountriesText)
+	elYear.textContent = yearText
+	elNumCountries.textContent = numCountriesText
+}
+
+// Returns a set of unique countries.
+const getUniqueCountries = (data) => {
+	// Get a set of all unique countries.
+	// Start by initializing aforementioned set.
+	const countries = new Set()
+	// Reducer gets ran recursively for each entry
+	// in the data array, which is called with reduce.
+	const reducer = (accumulator, entry) => {
+		accumulator.add(entry['Country or region'])
+		return accumulator
+	}
+	// Countries is the default start-value for the
+	// accumulator, which gets modified/filled.
+	data.reduce(reducer, countries)
+	return countries
 }
 
 const StudyHappiness = (year) => {
+	// Find file from input year.
 	const file = `./data/${year}.json`
 
 	loadJSON(file, (response) => {
 		// Parse JSON string into object.
 		const data = JSON.parse(response)
 
-		// Get a set of all unique countries.
-		// Start by initializing aforementioned set.
-		const countries = new Set()
-		// Reducer gets ran recursively for each entry
-		// in the data array, which is called with reduce.
-		const reducer = (accumulator, entry) => {
-			accumulator.add(entry['Country or region'])
-			return accumulator
-		}
-		// Countries is the default start-value for the
-		// accumulator, which gets modified/filled.
-		data.reduce(reducer, countries)
+		// Gather data from helper functions.
+		const countries = getUniqueCountries(data)
 
 		// Populate elements from earlier construction.
 		populateEls({
